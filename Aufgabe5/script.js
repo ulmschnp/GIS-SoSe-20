0b8d5cc165c935a26f844244e21d4ee21043f002
Ulmschneider <patrick.ulmschneider@hs-furtwangen.de>
2020-05-30 23:35:35 +0200

    aufgabe5 js

---
+++ Aufgabe5/script.js
@@ -0,0 +1,202 @@
+"use strict";
+let luiSupJacke = {
+    ueberschrift: "LuisVitton Supreme Jacke",
+    bild: "luisupjacke.jpg",
+    beschreibung: "Wer zuerst kommt, der hats verdient",
+    preis: "13 899€"
+};
+let luisupbag = {
+    ueberschrift: "LuisVitton Supreme Bag RED",
+    bild: "Louis-Vuitton-Supreme.jpg",
+    beschreibung: "Genug Platz für arme Studenten",
+    preis: "16 899€"
+};
+let phillipPlainJacket = {
+    ueberschrift: "PhillipPlainJacket",
+    bild: "phillipplainjacke.jpg",
+    beschreibung: "Entweder oder!",
+    preis: "1989,00€"
+};
+let phillipplainhose = {
+    ueberschrift: "PhillipPlain Jeans",
+    bild: "phillipplainhose.jpg",
+    beschreibung: "Bequemer als man denkt",
+    preis: "904,90€"
+};
+let phillipPlainWeste = {
+    ueberschrift: "PhillipPlain Weste",
+    bild: "ppweste.jpg",
+    beschreibung: "Flexen!",
+    preis: "900,07€"
+};
+let gcs = {
+    ueberschrift: "GucciSupreme Jogger",
+    bild: "guccisupjogger.jpg",
+    beschreibung: "Nur noch Gucci Bratan",
+    preis: "980,00€"
+};
+let offWhite = {
+    ueberschrift: "OffWhite Gürtel",
+    bild: "offwhiteG.jpeg",
+    beschreibung: "AirPort Lifestyle",
+    preis: "150,99€"
+};
+let gucciCap = {
+    ueberschrift: "Gucci Cap 2.0",
+    bild: "gucciCap.jpg",
+    beschreibung: "Perfekt für den Sommer",
+    preis: "250,99€"
+};
+let offWhitePulli = {
+    ueberschrift: "OffWhite Pulli",
+    bild: "offwhitePulli.jpg",
+    beschreibung: "lalalalalalal",
+    preis: "650,99€"
+};
+let gucciLO = {
+    ueberschrift: "Gucci SurpriseBox",
+    bild: "gucciLO.png",
+    beschreibung: "mind. 2 GucciArtikel",
+    preis: "1500,00€"
+};
+let stoneIslandhoddy = {
+    ueberschrift: "StoneIsland Pulli",
+    bild: "stoneIslandhoddy.jpg",
+    beschreibung: "Der Klassiker!",
+    preis: "589,98€"
+};
+let stoneisland = {
+    ueberschrift: "StoneIsland Shirt",
+    bild: "stoneisland.jpg",
+    beschreibung: "Gönn Dir!",
+    preis: "238,00€"
+};
+let specialedition = [luiSupJacke, luisupbag, phillipPlainJacket, phillipplainhose,
+    phillipPlainWeste, gcs, offWhite, gucciCap, offWhitePulli, gucciLO, stoneIslandhoddy, stoneisland];
+let nike90offwhite = {
+    ueberschrift: "Nike 90 OffWhite BLACK",
+    bild: "offwhiot90.jpg",
+    beschreibung: "Der legendäre 90er AirMax mit zusammenarbeit von OffWhite in schönstem Schwarz",
+    preis: "699,99€"
+};
+let offWhiteS = {
+    ueberschrift: "90 Airmax OffWhite",
+    bild: "offwhiot90.jpg",
+    beschreibung: "Skurr Skurr",
+    preis: "1899€"
+};
+let airJordanBLUEBLACK = {
+    ueberschrift: "Air Jordan BLUE&BLACK",
+    bild: "airJordanblueblack.jpg",
+    beschreibung: "darf es etwas auffälliger werden, dann ist dieser Schuh der Richtige für dich!",
+    preis: "999,00€"
+};
+let nikebacktofuture = {
+    ueberschrift: "Nike 720 back to future 2019",
+    bild: "nike720backfuture.jpg",
+    beschreibung: "Ein echtes Sammlersück für den kleinen Mann",
+    preis: "280€"
+};
+let nikebj = {
+    ueberschrift: "Nike Ben & Jerry Edition",
+    bild: "nikeBenandJerrys.jpeg",
+    beschreibung: "Für echte Eis Liebhaber!",
+    preis: "200,07€"
+};
+let airMAG = {
+    ueberschrift: "Nike Nike AirMAG",
+    bild: "nikeAirmag.jpg",
+    beschreibung: "Schonmal den Filmklassiker zurück in die Zukunft geschaut?",
+    preis: "16980,00€"
+};
+let bts = {
+    ueberschrift: "Balenciaga TripleS BLACK",
+    bild: "balenciagaTripleSblack.jpg",
+    beschreibung: "Die ersten Wochen etwas unbequem, dann aber ein Traum",
+    preis: "750,99€"
+};
+let btsw = {
+    ueberschrift: "Balenciaga TripleS WHITE",
+    bild: "balenciageTripleSwhite.jpg",
+    beschreibung: "Für den kleinen Geldbeutel",
+    preis: "750,99€"
+};
+let yeezyBoost = {
+    ueberschrift: "YeezyBOOST350 V2 Zebra",
+    bild: "yeezyboost350v2zebra.jpg",
+    beschreibung: "Für den FLEX",
+    preis: "850,99€"
+};
+let yeezy = {
+    ueberschrift: "YeezyBOOST350 V2",
+    bild: "gucciLO.png",
+    beschreibung: "mind. 2 GucciArtikel",
+    preis: "1000,00€"
+};
+let ys = {
+    ueberschrift: "Yeezy SUPREME",
+    bild: "yeezysupreme.jpg",
+    beschreibung: "SammlerSTÜCK",
+    preis: "6589,98€"
+};
+let nSup = {
+    ueberschrift: "Nike SUPREME",
+    bild: "NIKEsupreme.jpeg",
+    beschreibung: "Hast du Ihn schon?",
+    preis: "5676,99€"
+};
+let shoes = [nike90offwhite, offWhiteS, airJordanBLUEBLACK, nikebacktofuture,
+    nikebj, airMAG, bts, btsw, offWhitePulli, yeezyBoost, yeezy, ys, nSup];
+for (let zahl = 0; zahl < specialedition.length; zahl++) {
+    let nDiv = document.createElement("div");
+    nDiv.id = "Special" + zahl;
+    nDiv.setAttribute("class", "box");
+    document.getElementById("Kategorie1")?.appendChild(nDiv);
+    //NAME
+    let headline = document.createElement("p");
+    headline.innerHTML = specialedition[zahl].ueberschrift;
+    document.getElementById("Special" + zahl)?.appendChild(headline);
+    //IMG
+    let img = document.createElement("img");
+    img.src = specialedition[zahl].bild;
+    document.getElementById("Special" + zahl)?.appendChild(img);
+    //DESCRIPTION
+    let content = document.createElement("p");
+    content.innerHTML = specialedition[zahl].beschreibung;
+    document.getElementById("Special" + zahl)?.appendChild(content);
+    //PREIS
+    let price = document.createElement("p");
+    price.innerHTML = specialedition[zahl].preis;
+    document.getElementById("Special" + zahl)?.appendChild(price);
+    //BUY
+    let einkaufen = document.createElement("button");
+    einkaufen.innerHTML = "Einkaufswagen";
+    document.getElementById("Special" + zahl)?.appendChild(einkaufen);
+}
+for (let zahl = 0; zahl < shoes.length; zahl++) {
+    let nDiv = document.createElement("div");
+    nDiv.id = "Special" + zahl;
+    nDiv.setAttribute("class", "box");
+    document.getElementById("Kategorie2")?.appendChild(nDiv);
+    //Name
+    let headline = document.createElement("p");
+    headline.innerHTML = specialedition[zahl].ueberschrift;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(headline);
+    //bild
+    let img = document.createElement("img");
+    img.src = specialedition[zahl].bild;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(img);
+    //Beschreibung
+    let content = document.createElement("p");
+    content.innerHTML = specialedition[zahl].beschreibung;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(content);
+    //PREIS
+    let price = document.createElement("p");
+    price.innerHTML = specialedition[zahl].preis;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(price);
+    //Einkaufswagen
+    let kaufen = document.createElement("button");
+    kaufen.innerHTML = "Einkaufswagen";
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(kaufen);
+}
+//# sourceMappingURL=script.js.map
---
+++ Aufgabe5/script.ts
@@ -0,0 +1,270 @@
+interface Special {
+    ueberschrift: string;
+    bild: string;
+    beschreibung: string;
+    preis: string;
+
+}
+let luiSupJacke: Special = {
+    ueberschrift: "LuisVitton Supreme Jacke",
+    bild: "luisupjacke.jpg",
+    beschreibung: "Wer zuerst kommt, der hats verdient",
+    preis: "13 899€"
+};
+
+
+let luisupbag: Special = {
+    ueberschrift: "LuisVitton Supreme Bag RED",
+    bild: "Louis-Vuitton-Supreme.jpg",
+    beschreibung: "Genug Platz für arme Studenten",
+    preis: "16 899€"
+};
+
+let phillipPlainJacket: Special = {
+    ueberschrift: "PhillipPlainJacket",
+    bild: "phillipplainjacke.jpg",
+    beschreibung: "Entweder oder!",
+    preis: "1989,00€"
+};
+
+let phillipplainhose: Special = {
+    ueberschrift: "PhillipPlain Jeans",
+    bild: "phillipplainhose.jpg",
+    beschreibung: "Bequemer als man denkt",
+    preis: "904,90€"
+};
+
+let phillipPlainWeste: Special = {
+    ueberschrift: "PhillipPlain Weste",
+    bild: "ppweste.jpg",
+    beschreibung: "Flexen!",
+    preis: "900,07€"
+};
+
+let gcs: Special = {
+    ueberschrift: "GucciSupreme Jogger",
+    bild: "guccisupjogger.jpg",
+    beschreibung: "Nur noch Gucci Bratan",
+    preis: "980,00€"
+};
+
+let offWhite: Special = {
+    ueberschrift: "OffWhite Gürtel",
+    bild: "offwhiteG.jpeg",
+    beschreibung: "AirPort Lifestyle",
+    preis: "150,99€"
+};
+
+let gucciCap: Special = {
+    ueberschrift: "Gucci Cap 2.0",
+    bild: "gucciCap.jpg",
+    beschreibung: "Perfekt für den Sommer",
+    preis: "250,99€"
+};
+
+
+let offWhitePulli: Special = {
+    ueberschrift: "OffWhite Pulli",
+    bild: "offwhitePulli.jpg",
+    beschreibung: "lalalalalalal",
+    preis: "650,99€"
+};
+
+let gucciLO: Special = {
+    ueberschrift: "Gucci SurpriseBox",
+    bild: "gucciLO.png",
+    beschreibung: "mind. 2 GucciArtikel",
+    preis: "1500,00€"
+};
+
+let stoneIslandhoddy: Special = {
+    ueberschrift: "StoneIsland Pulli",
+    bild: "stoneIslandhoddy.jpg",
+    beschreibung: "Der Klassiker!",
+    preis: "589,98€"
+};
+
+
+let stoneisland: Special = {
+    ueberschrift: "StoneIsland Shirt",
+    bild: "stoneisland.jpg",
+    beschreibung: "Gönn Dir!",
+    preis: "238,00€"
+};
+
+interface SpecialShoes {
+        ueberschrift: string;
+        bild: string;
+        beschreibung: string;
+        preis: string;
+    
+    }
+let nike90offwhite: SpecialShoes = {
+        ueberschrift: "Nike 90 OffWhite BLACK",
+        bild: "offwhiot90.jpg",
+        beschreibung: "Der legendäre 90er AirMax mit zusammenarbeit von OffWhite in schönstem Schwarz",
+        preis: "699,99€"
+    };
+    
+    
+let offWhiteS: SpecialShoes = {
+        ueberschrift: "90 Airmax OffWhite",
+        bild: "offwhiot90.jpg",
+        beschreibung: "Skurr Skurr",
+        preis: "1899€"
+    };
+    
+let airJordanBLUEBLACK: SpecialShoes = {
+        ueberschrift: "Air Jordan BLUE&BLACK",
+        bild: "airJordanblueblack.jpg",
+        beschreibung: "darf es etwas auffälliger werden, dann ist dieser Schuh der Richtige für dich!",
+        preis: "999,00€"
+    };
+    
+let nikebacktofuture: SpecialShoes = {
+        ueberschrift: "Nike 720 back to future 2019",
+        bild: "nike720backfuture.jpg",
+        beschreibung: "Ein echtes Sammlersück für den kleinen Mann",
+        preis: "280€"
+    };
+    
+let nikebj: SpecialShoes = {
+        ueberschrift: "Nike Ben & Jerry Edition",
+        bild: "nikeBenandJerrys.jpeg",
+        beschreibung: "Für echte Eis Liebhaber!",
+        preis: "200,07€"
+    };
+    
+let airMAG: SpecialShoes = {
+        ueberschrift: "Nike Nike AirMAG",
+        bild: "nikeAirmag.jpg",
+        beschreibung: "Schonmal den Filmklassiker zurück in die Zukunft geschaut?",
+        preis: "16980,00€"
+    };
+    
+let bts: SpecialShoes = {
+        ueberschrift: "Balenciaga TripleS BLACK",
+        bild: "balenciagaTripleSblack.jpg",
+        beschreibung: "Die ersten Wochen etwas unbequem, dann aber ein Traum",
+        preis: "750,99€"
+    };
+    
+let btsw: SpecialShoes = {
+        ueberschrift: "Balenciaga TripleS WHITE",
+        bild: "balenciageTripleSwhite.jpg",
+        beschreibung: "Für den kleinen Geldbeutel",
+        preis: "750,99€"
+    };
+    
+    
+let yeezyBoost: SpecialShoes = {
+        ueberschrift: "YeezyBOOST350 V2 Zebra",
+        bild: "yeezyboost350v2zebra.jpg",
+        beschreibung: "Für den FLEX",
+        preis: "850,99€"
+    };
+    
+let yeezy: SpecialShoes = {
+        ueberschrift: "YeezyBOOST350 V2",
+        bild: "gucciLO.png",
+        beschreibung: "mind. 2 GucciArtikel",
+        preis: "1000,00€"
+    };
+    
+let ys: SpecialShoes = {
+        ueberschrift: "Yeezy SUPREME",
+        bild: "yeezysupreme.jpg",
+        beschreibung: "SammlerSTÜCK",
+        preis: "6589,98€"
+    };
+    
+    
+let nSup: SpecialShoes = {
+        ueberschrift: "Nike SUPREME",
+        bild: "NIKEsupreme.jpeg",
+        beschreibung: "Hast du Ihn schon?",
+        preis: "5676,99€"
+    };
+    
+
+let specialedition: Special[] = [luiSupJacke, luisupbag, phillipPlainJacket, phillipplainhose, 
+        phillipPlainWeste, gcs, offWhite, gucciCap, offWhitePulli, gucciLO, stoneIslandhoddy, stoneisland];
+
+
+
+let shoes: SpecialShoes[] = [nike90offwhite, offWhiteS, airJordanBLUEBLACK, nikebacktofuture, 
+        nikebj, airMAG, bts, btsw, offWhitePulli, yeezyBoost, yeezy, ys, nSup];
+
+
+
+    
+for (let zahl: number = 0; zahl < specialedition.length; zahl++) {
+    
+    let nDiv: HTMLDivElement = document.createElement("div");
+    nDiv.id = "Special" + zahl;
+    nDiv.setAttribute("class", "box");
+    document.getElementById("Kategorie1")?.appendChild(nDiv);
+
+    let headline: HTMLParagraphElement = document.createElement("p");    //Name
+    headline.innerHTML = specialedition[zahl].ueberschrift;
+    document.getElementById("Special" + zahl)?.appendChild(headline);
+
+   
+    let img: HTMLImageElement = document.createElement("img");     //Bild
+    img.src = specialedition[zahl].bild;
+    document.getElementById("Special" + zahl)?.appendChild(img);
+
+   
+    let content: HTMLParagraphElement = document.createElement("p");    //Beschreib.
+    content.innerHTML = specialedition[zahl].beschreibung;
+    document.getElementById("Special" + zahl)?.appendChild(content);
+
+   
+    let price: HTMLParagraphElement = document.createElement("p");    //Preis
+    price.innerHTML = specialedition[zahl].preis;
+    document.getElementById("Special" + zahl)?.appendChild(price);
+
+    
+    let einkaufen: HTMLButtonElement = document.createElement("button"); //Kaufen
+    einkaufen.innerHTML = "Einkaufswagen";
+    document.getElementById("Special" + zahl)?.appendChild(einkaufen);
+}
+// ---------------------------for--------------------------------------------------------
+for (let zahl: number = 0; zahl < shoes.length; zahl++) {
+    
+    let nDiv: HTMLDivElement = document.createElement("div");
+    nDiv.id = "Special" + zahl;
+    nDiv.setAttribute("class", "box");
+    document.getElementById("Kategorie2")?.appendChild(nDiv);
+
+   
+    let headline: HTMLParagraphElement = document.createElement("p");    //Name
+    headline.innerHTML = specialedition[zahl].ueberschrift;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(headline);
+
+   
+    let img: HTMLImageElement = document.createElement("img");    //bild
+    img.src = specialedition[zahl].bild;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(img);
+
+   
+    let content: HTMLParagraphElement = document.createElement("p");   //Beschreibung
+    content.innerHTML = specialedition[zahl].beschreibung;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(content);
+
+    
+    let price: HTMLParagraphElement = document.createElement("p");   //Preis
+    price.innerHTML = specialedition[zahl].preis;
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(price);
+
+   
+    let kaufen: HTMLButtonElement = document.createElement("button");    //Einkaufswagen
+    kaufen.innerHTML = "Einkaufswagen";
+    document.getElementById("SpecialShoes" + zahl)?.appendChild(kaufen);
+}
+
+
+
+
+    
+        
