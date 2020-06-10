b71084ac4861a290c33088ebdf9556fe52d68235
Ulmschneider <patrick.ulmschneider@hs-furtwangen.de>
2020-06-10 08:17:54 +0200

    verbesserung

---
+++ Aufgabe6/shoes.ts
@@ -0,0 +1,340 @@
+// intaface fürs erte Kategorie
+namespace namespace06 {
+    interface Artikel {
+        bild: string;
+        name: string;
+        beschreibung: string;
+        preis: number;
+        kategorie: string;
+
+    }
+
+    let luiSupJacke: Artikel = {
+        name: "LuisVitton Supreme Jacke",
+        bild: "luisupjacke.jpg",
+        beschreibung: "Wer zuerst kommt, der hats verdient",
+        preis: 13.899,
+        kategorie: "specials"
+
+    };
+
+
+    let luisupbag: Artikel = {
+        name: "LuisVitton Supreme Bag RED",
+        bild: "Louis-Vuitton-Supreme.jpg",
+        beschreibung: "Genug Platz für arme Studenten",
+        preis: 16899,
+        kategorie: "specials"
+    };
+
+    let phillipPlainJacket: Artikel = {
+        name: "PhillipPlainJacket",
+        bild: "phillipplainjacke.jpg",
+        beschreibung: "Entweder oder!",
+        preis: 1989,
+        kategorie: "specials"
+    };
+
+    let phillipplainhose: Artikel = {
+        name: "PhillipPlain Jeans",
+        bild: "phillipplainhose.jpg",
+        beschreibung: "Bequemer als man denkt",
+        preis: 904.90,
+        kategorie: "specials"
+    };
+
+    let phillipPlainWeste: Artikel = {
+        name: "PhillipPlain Weste",
+        bild: "ppweste.jpg",
+        beschreibung: "Flexen!",
+        preis: 900.07,
+        kategorie: "specials"
+    };
+
+    let gcs: Artikel = {
+        name: "GucciSupreme Jogger",
+        bild: "guccisupjogger.jpg",
+        beschreibung: "Nur noch Gucci Bratan",
+        preis: 980,
+        kategorie: "specials"
+    };
+
+    let offWhite: Artikel = {
+        name: "OffWhite Gürtel",
+        bild: "offwhiteG.jpeg",
+        beschreibung: "AirPort Lifestyle",
+        preis: 150.99,
+        kategorie: "specials"
+    };
+
+    let gucciCap: Artikel = {
+        name: "Gucci Cap 2.0",
+        bild: "gucciCap.jpg",
+        beschreibung: "Perfekt für den Sommer",
+        preis: 250.99,
+        kategorie: "specials"
+    };
+
+
+    let offWhitePulli: Artikel = {
+        name: "OffWhite Pulli",
+        bild: "offwhitePulli.jpg",
+        beschreibung: "lalalalalalal",
+        preis: 650.99,
+        kategorie: "specials"
+    };
+
+    let gucciLO: Artikel = {
+        name: "Gucci SurpriseBox",
+        bild: "gucciLO.png",
+        beschreibung: "mind. 2 GucciArtikel",
+        preis: 1500,
+        kategorie: "specials"
+    };
+
+    let stoneIslandhoddy: Artikel = {
+        name: "StoneIsland Pulli",
+        bild: "stoneIslandhoddy.jpg",
+        beschreibung: "Der Klassiker!",
+        preis: 589.98,
+        kategorie: "specials"
+    };
+
+
+    let stoneisland: Artikel = {
+        name: "StoneIsland Shirt",
+        bild: "stoneisland.jpg",
+        beschreibung: "Gönn Dir!",
+        preis: 238,
+        kategorie: "specials"
+    };
+
+
+
+    let nike90offwhite: Artikel = {
+        name: "Nike 90 OffWhite BLACK",
+        bild: "offwhiot90.jpg",
+        beschreibung: "Der legendäre 90er AirMax mit zusammenarbeit von OffWhite in schönstem Schwarz",
+        preis: 699.99,
+        kategorie: "schuhe"
+    };
+
+
+    let offWhiteS: Artikel = {
+        name: "90 Airmax OffWhite",
+        bild: "offwhiot90.jpg",
+        beschreibung: "Skurr Skurr",
+        preis: 1899,
+        kategorie: "schuhe"
+    };
+
+    let airJordanBLUEBLACK: Artikel = {
+        name: "Air Jordan BLUE&BLACK",
+        bild: "airJordanblueblack.jpg",
+        beschreibung: "darf es etwas auffälliger werden, dann ist dieser Schuh der Richtige für dich!",
+        preis: 999.00,
+        kategorie: "schuhe"
+    };
+
+    let nikebacktofuture: Artikel = {
+        name: "Nike 720 back to future 2019",
+        bild: "nike720backfuture.jpg",
+        beschreibung: "Ein echtes Sammlersück für den kleinen Mann",
+        preis: 280,
+        kategorie: "schuhe"
+    };
+
+    let nikebj: Artikel = {
+        name: "Nike Ben & Jerry Edition",
+        bild: "nikeBenandJerrys.jpeg",
+        beschreibung: "Für echte Eis Liebhaber!",
+        preis: 200.07,
+        kategorie: "schuhe"
+    };
+
+    let airMAG: Artikel = {
+        name: "Nike Nike AirMAG",
+        bild: "nikeAirmag.jpg",
+        beschreibung: "Schonmal den Filmklassiker zurück in die Zukunft geschaut?",
+        preis: 16980.00,
+        kategorie: "schuhe"
+    };
+
+    let bts: Artikel = {
+        name: "Balenciaga TripleS BLACK",
+        bild: "balenciagaTripleSblack.jpg",
+        beschreibung: "Die ersten Wochen etwas unbequem, dann aber ein Traum",
+        preis: 750.99,
+        kategorie: "schuhe"
+    };
+
+    let btsw: Artikel = {
+        name: "Balenciaga TripleS WHITE",
+        bild: "balenciageTripleSwhite.jpg",
+        beschreibung: "Für den kleinen Geldbeutel",
+        preis: 750.99,
+        kategorie: "schuhe"
+    };
+
+
+    let yeezyBoost: Artikel = {
+        name: "YeezyBOOST350 V2 Zebra",
+        bild: "yeezyboost350v2zebra.jpg",
+        beschreibung: "Für den FLEX",
+        preis: 850.99,
+        kategorie: "schuhe"
+    };
+
+    let yeezy: Artikel = {
+        name: "YeezyBOOST350 V2",
+        bild: "gucciLO.png",
+        beschreibung: "mind. 2 GucciArtikel",
+        preis: 1000.00,
+        kategorie: "schuhe"
+    };
+
+    let ys: Artikel = {
+        name: "Yeezy SUPREME",
+        bild: "yeezysupreme.jpg",
+        beschreibung: "SammlerSTÜCK",
+        preis: 6589.98,
+        kategorie: "schuhe"
+    };
+
+
+    let nSup: Artikel = {
+        name: "Nike SUPREME",
+        bild: "NIKEsupreme.jpeg",
+        beschreibung: "Hast du Ihn schon?",
+        preis: 5676.99,
+        kategorie: "schuhe"
+
+    };
+
+
+    // atickel is array geladen 
+    let artikel: Artikel[] = [luiSupJacke, luisupbag, luisupbag, phillipPlainJacket, phillipplainhose, phillipPlainWeste, gcs, offWhite, gucciCap, offWhitePulli, gucciLO, stoneIslandhoddy, stoneisland, nike90offwhite, offWhiteS, airJordanBLUEBLACK, nikebacktofuture, nikebj, airMAG, bts, btsw, yeezyBoost, yeezy, ys, nSup];
+    let kategorien: string[] = ["special", "schuhe"];
+
+    // schleife zum seite generieren 
+    for (let index: number = 0; index < artikel.length; index++) {
+
+        let newDiv: HTMLDivElement = document.createElement("div");
+        newDiv.id = "Artikel" + index;
+        newDiv.setAttribute("class", "box");
+        document.getElementById("Kategorie1")?.appendChild(newDiv);
+
+        let newH: HTMLParagraphElement = document.createElement("h2");    //Name
+        newH.innerHTML = artikel[index].name;
+        document.getElementById("Artikel" + index)?.appendChild(newH);
+
+
+        let newImg: HTMLImgElement = document.createElement("img");     //Bild
+        newImg.src = artikel[index].bild;
+        document.getElementById("Artikel" + index)?.appendChild(newImg);
+
+
+        let newP: HTMLParagraphElement = document.createElement("p");    //Beschreib.
+        newP.innerHTML = artikel[index].beschreibung;
+        document.getElementById("Artikel" + index)?.appendChild(newP);
+
+
+        let newPreis: HTMLParagraphElement = document.createElement("p");    //Preis
+        newPreis.innerHTML = artikel[index].preis.toFixed(2) + "€";
+        document.getElementById("Artikel" + index)?.appendChild(newPreis);
+
+
+        let newB: HTMLButtonElement = document.createElement("button"); //Kaufen
+        newB.value = "Kaufen";
+        newB.type = "submit";
+        document.getElementById("Artikel" + index)?.appendChild(newB);
+        newB.addEventListener("click", kaufen);
+    }
+
+
+    // zaehler variable
+    let anzahl: number = 0;
+    let summe: number = 0;
+
+    // element für sichtbarkeit der Eingelagerten Produkte
+    let newZ: HTMLDivElement = document.createElement("div");
+    newZ.id = "anz";
+
+
+
+    // Funktion fur den Kaufbutton 
+    function kaufen(_event: Event): void {
+
+        //einbelenden des Kreises 
+        if (anzahl < 1) {
+            document.getElementById("header")?.appendChild(newZ);
+        }
+        anzahl += 1;
+        newZ.innerHTML = "" + anzahl;
+
+        //zusammenrechnen der presie 
+        let test: string = (<HTMLDivElement>(<HTMLElement>_event.currentTarget).parentElement).getAttribute("id")!;
+        let test2: string[] = test.split("artikel");
+        console.log(parseInt(test2[1]));
+        // Summe der Preise  
+        summe += artikel[parseInt(test2[1])].preis;
+        console.log(summe + "€");
+
+    }
+
+
+    // ersellung der navigation
+    let newul: HTMLUListElement = document.createElement("ul");
+    document.getElementById("topmenu")?.appendChild(newul);
+    for (let index: number = 0; index < kategorien.length + 1; index++) {
+        let newli: HTMLLIElement = document.createElement("li");
+        let newa: HTMLAnchorElement = document.createElement("a");
+        newa.href = "#";
+        newa.id = "a" + index;
+
+        if (kategorien.length > index) {
+            newa.innerHTML = kategorien[index];
+        } else {
+            newa.innerHTML = "Alles";
+        }
+        newul.appendChild(newli);
+        newli.appendChild(newa);
+        newa.addEventListener("click", springen);
+    }
+
+    // Ausund einblede Event
+    function springen(_event: Event): void {
+        //alle möglichkeiten für kategorien newue mussen per hand nachgereicht werden
+        switch ((<HTMLAnchorElement>_event.currentTarget).getAttribute("id")) {
+            case "a0":
+                //nur Büroanzeigen
+                (<HTMLDivElement>document.getElementById("kategorie1")).style.display = "flex";
+                (<HTMLDivElement>document.getElementById("kategorie2")).style.display = "none";
+                (<HTMLElement>document.getElementById("k1")).style.display = "flex";
+                (<HTMLElement>document.getElementById("k2")).style.display = "none";
+                console.log("Special");
+                break;
+            case "a1":
+                //nur Gartenartikel anzeigfen
+                (<HTMLDivElement>document.getElementById("kategorie1")).style.display = "none";
+                (<HTMLDivElement>document.getElementById("kategorie2")).style.display = "flex";
+                (<HTMLElement>document.getElementById("k1")).style.display = "none";
+                (<HTMLElement>document.getElementById("k2")).style.display = "flex";
+                console.log("Shoes");
+                break;
+            case "a2":
+                //alles anzeigen
+                (<HTMLDivElement>document.getElementById("kategorie1")).style.display = "flex";
+                (<HTMLDivElement>document.getElementById("kategorie2")).style.display = "flex";
+                (<HTMLElement>document.getElementById("k1")).style.display = "flex";
+                (<HTMLElement>document.getElementById("k2")).style.display = "flex";
+                console.log("Alle");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+}
